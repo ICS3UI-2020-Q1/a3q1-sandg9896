dfa279eb42cbdb8561ccf09662e10b028cbf9970
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 11:11:10 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Determine the greatest number
+ * @author Gavin
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+   // ask user to enter three numbers
+    System.out.print("Please enter a number:");
+    int number1 = input.nextInt();
+
+    System.out.print("Please enter another number:");
+    int number2 = input.nextInt();
+    
+    System.out.print("Please enter a final number:");
+    int number3 = input.nextInt();
+
+    // Determine the greatest number
+    if(number1>number2 && number1>number3){
+    System.out.print("Maximum: " + number1);
+    }
+    if(number2>number1 && number2>number3){
+    System.out.print("Maximum: " + number2);
+    }
+    if(number3>number2 && number3>number1){
+    System.out.print("Maximum: " + number3);
+    }
+
     
   }
 }
